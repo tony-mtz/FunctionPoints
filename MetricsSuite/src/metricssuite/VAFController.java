2d57ddeb78a86ce1fde329b127223eb7cee8b4fc
nickjmiller <nickamilli@gmail.com>
2018-02-14 18:32:23 -0800

    Added VAF window labels and choiceboxes. Changed VAF to array.

--- MetricsSuite/src/metricssuite/VAFController.java
+++ MetricsSuite/src/metricssuite/VAFController.java
@@ -1,39 +1,24 @@
 package metricssuite;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import utils.ValueFactor;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class VAFController implements Initializable {
 
     @FXML
-    private TableColumn<ValueFactor, String> descCol;
+    private Button cancel;
     @FXML
-    private TableColumn<ValueFactor, Integer> ratingCol;
-    @FXML
-    private TableView table;
-    private final ObservableList<ValueFactor> data =
-            FXCollections.observableArrayList(
-                    new ValueFactor("test", 1),
-                    new ValueFactor("t2", 5)
-            );
-
+    private void close(){
+        Stage stage = (Stage) cancel.getScene().getWindow();
+        stage.close();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        descCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
-        ratingCol.setCellValueFactory(new PropertyValueFactory<>("value"));
-        table.setItems(data);
+
     }
 }
--- MetricsSuite/src/utils/Vaf.java
+++ MetricsSuite/src/utils/Vaf.java
@@ -11,38 +11,13 @@ package utils;
  */
 public class Vaf {
     
-    public int qestion1;
-    public int qestion2;
-    public int qestion3;
-    public int qestion4;
-    public int qestion5;
-    public int qestion6;
-    public int qestion7;
-    public int qestion8;
-    public int qestion9;
-    public int qestion10;
-    public int qestion11;
-    public int qestion12;
-    public int qestion13;
-    public int qestion14;
-    public int qestion15;
-    
+    private int[] values = new int[14];
+
     public Vaf(){
-        this.qestion1 = 0;
-        this.qestion2 = 0;
-        this.qestion3 = 0;
-        this.qestion4 = 0;
-        this.qestion5 = 0;
-        this.qestion6 = 0;
-        this.qestion7 = 0;
-        this.qestion8 = 0;
-        this.qestion9 = 0;
-        this.qestion10 = 0;
-        this.qestion11 = 0;
-        this.qestion12 = 0;
-        this.qestion13 = 0;
-        this.qestion14 = 0;
-        this.qestion15 = 0;
+    }
+
+    public Vaf(int[] values){
+        this.values = values;
                 
     }
     
